35bfd8ee1fc01b8e21e1f953cdd4ffc6203a6d8b
Sophie Yawei Xi <xi.sophie@yahoo.fr>
2023-11-14 18:23:24 +0000

    exo 1 - day 2 - done

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,8 +1,8 @@
+{{ config(materialized='table') }}
 SELECT
     date_date
     ,COUNT(orders_id) AS nb_transactions
     ,ROUND(SUM(revenue),0) AS revenue 
-    ,ROUND(AVG(revenue),1) AS average_basket
     ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
     ,ROUND(SUM(margin),0) AS margin 
     ,ROUND(SUM(operational_margin),0) AS operational_margin 
